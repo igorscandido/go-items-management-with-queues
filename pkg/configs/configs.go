e45ec2b80754214e7b48971e35505e85d81f3631
igorcandido <igorcandido@proton.me>
2025-01-03 13:48:39 -0300

    feature: added configuration with yaml and map to struct

--- pkg/configs/configs.go
+++ pkg/configs/configs.go
@@ -1,5 +1,36 @@
 package configs
 
-func test() {
+import (
+	"log"
+	"os"
 
+	"gopkg.in/yaml.v3"
+)
+
+type rabbitMQ struct {
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Address  string `yaml:"address"`
+	Port     int    `yaml:"port"`
+	VHost    string `yaml:"vhost"`
+}
+
+type Configs struct {
+	RabbitMQ rabbitMQ `yaml:"rabbitmq"`
+}
+
+func NewConfigs() *Configs {
+	file, err := os.Open("environment.yaml")
+	if err != nil {
+		log.Fatalf("Failed to open config file: %v", err)
+	}
+	defer file.Close()
+
+	var cfg Configs
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(&cfg); err != nil {
+		log.Fatalf("Failed to decode YAML file: %v", err)
+	}
+
+	return &cfg
 }
