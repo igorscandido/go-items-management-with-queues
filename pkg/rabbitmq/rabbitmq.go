76b1b320a3fc7782a2e82648acebb7e1c3a694b5
igorcandido <igorcandido@proton.me>
2025-01-04 16:16:01 -0300

    feature: added retry to connect on rabbitmq

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -2,12 +2,13 @@ package rabbitmq
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/igorscandido/go-items-management-with-queues/pkg/configs"
 	"github.com/streadway/amqp"
 )
 
-func NewRabbitMQConnection(configs *configs.Configs) (*amqp.Connection, error) {
+func NewRabbitMQConnection(configs *configs.Configs) *amqp.Connection {
 	url := fmt.Sprintf("amqp://%s:%s@%s:%d%s",
 		configs.RabbitMQ.User,
 		configs.RabbitMQ.Password,
@@ -15,10 +16,17 @@ func NewRabbitMQConnection(configs *configs.Configs) (*amqp.Connection, error) {
 		configs.RabbitMQ.Port,
 		configs.RabbitMQ.VHost,
 	)
-	conn, err := amqp.Dial(url)
-	if err != nil {
-		return nil, err
+	var conn *amqp.Connection
+	for i := 0; i < configs.RabbitMQ.RetryPolicy.MaxRetries; i++ {
+		var err error
+		conn, err = amqp.Dial(url)
+		if err != nil {
+			fmt.Printf("Failed to connect to RabbitMQ: %v\n", err)
+			time.Sleep(time.Duration(configs.RabbitMQ.RetryPolicy.Interval) * time.Second)
+			continue
+		}
+		break
 	}
 
-	return conn, nil
+	return conn
 }
